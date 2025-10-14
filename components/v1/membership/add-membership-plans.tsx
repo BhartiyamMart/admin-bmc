7488c1e6e867ebd31ee1ec131c57f601e5fc339c
Gulshan <gulshany7669@gmail.com>
2025-10-14 09:56:55 +0530

    update the create employee api

--- components/v1/membership/add-membership-plans.tsx
+++ components/v1/membership/add-membership-plans.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react';
 import Link from 'next/link';
-import { ChevronLeft} from 'lucide-react';
+import { ChevronLeft } from 'lucide-react';
 import { Switch } from '@radix-ui/react-switch';
 
 const AddMembershipPlans = () => {
@@ -11,12 +11,12 @@ const AddMembershipPlans = () => {
 
   return (
     <div className="flex min-h-screen justify-center p-4">
-      <div className="max-h-[89vh] w-full overflow-y-auto rounded-lg bg-sidebar p-4 shadow-lg">
+      <div className="bg-sidebar max-h-[89vh] w-full overflow-y-auto rounded-lg p-4 shadow-lg">
         <div className="mb-4 flex w-full items-center justify-between border-b pb-2">
           <h2 className="text-lg font-semibold">Add Membership</h2>
           <Link
             href="/membership/membership-plans-list"
-            className="flex cursor-pointer bg-primary text-background items-center gap-2 rounded px-3 py-2 text-sm transition"
+            className="bg-primary text-background flex cursor-pointer items-center gap-2 rounded px-3 py-2 text-sm transition"
           >
             <ChevronLeft className="h-4 w-4" /> Back to List
           </Link>
@@ -47,13 +47,22 @@ const AddMembershipPlans = () => {
                 <input
                   type="file"
                   name="file"
-                  className="w-full rounded border border-gray-300 px-3 py-1 text-sm file:mr-4 file:rounded file:border-0 file:bg-primary file:px-4 file:py-2 file:text-background"
+                  className="file:bg-primary cursor-pointer file:text-background w-full rounded border border-gray-300 px-3 py-1 text-sm file:mr-4 file:rounded file:border-0 file:px-4 file:py-2"
                 />
               </div>
             </div>
             <div>
-              <div>
-                <label className="mb-1 block font-medium">Color</label>
+              <label className="mb-1 block font-medium">Color</label>
+              <div className="flex items-center gap-2">
+                {/* Color picker */}
+                <input
+                  type="color"
+                  value={color || '#ffffff'}
+                  onChange={(e) => setColor(e.target.value)}
+                  className="h-10 w-10 cursor-pointer rounded border border-gray-300"
+                />
+
+                {/* Hex input */}
                 <input
                   type="text"
                   name="color"
@@ -70,6 +79,7 @@ const AddMembershipPlans = () => {
                 />
               </div>
             </div>
+
             <div>
               <label className="mb-1 block font-medium">Sort Order</label>
               <select name="sortorder" className="w-full rounded border px-3 py-3">
@@ -86,7 +96,7 @@ const AddMembershipPlans = () => {
                   id="isactive"
                   checked={isActive}
                   onCheckedChange={(checked) => setIsActive(checked)}
-                  className={`relative inline-flex h-6 w-11 items-center rounded-full transition-colors ${
+                  className={`relative inline-flex h-6 w-11 items-center rounded-full transition-colors cursor-pointer ${
                     isActive ? 'bg-orange-500' : 'bg-gray-300'
                   }`}
                 >
@@ -99,7 +109,7 @@ const AddMembershipPlans = () => {
               </div>
             </div>
           </div>
-          <button type="submit" className="mt-0 w-[320px] rounded bg-primary text-background py-2">
+          <button type="submit" className="bg-primary text-background mt-0 w-[320px] rounded py-2 cursor-pointer ">
             Save
           </button>
         </form>
