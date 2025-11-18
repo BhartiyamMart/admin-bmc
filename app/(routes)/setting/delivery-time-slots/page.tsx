a8b8c7f0fc422b37dc0a2a91e667364af897b75f
Gulshan <gulshany7669@gmail.com>
2025-11-18 09:49:24 +0530

    connect the link of pages according to backend urls

---
+++ app/(routes)/setting/delivery-time-slots/page.tsx
@@ -0,0 +1,12 @@
+import DeliveryTimeSlotList from '@/components/v1/delivery/delivery-time-slots'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <DeliveryTimeSlotList/>
+    </>
+  )
+}
+
+export default page
