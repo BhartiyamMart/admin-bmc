fb0151741e7df32e0b098df4da083b1a85efce74
rishika <singhrishika944@gmmail.com>
2026-01-03 10:21:31 +0530

    solved build error

--- app/(routes)/feedbacks/edit-feedback-category/page.tsx
+++ app/(routes)/feedbacks/edit-feedback-category/page.tsx
@@ -1,7 +1,7 @@
-// import EditFeedbackCategory from '@/components/v1/feedbacks/edit-feedback-category/edit-feedback';
+import EditFeedbackCategory from '@/components/v1/feedbacks/edit-feedback-category/edit-feedback';
 
-// const page = () => {
-//   return <EditFeedbackCategory />;
-// };
+const page = () => {
+  return <EditFeedbackCategory />;
+};
 
-// export default page;
+export default page;
